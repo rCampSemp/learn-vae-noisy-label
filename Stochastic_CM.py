8de191dab79c5232292d2a58949849b83761cd11
rCampSemp <semp.rhys@outlook.com>
2022-08-03 16:26:31 +0100

    2022 08 03 a: adds vae structure to cm network

--- Stochastic_CM.py
+++ Stochastic_CM.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torch.distributions import LowRankMultivariateNormal
+from torch.distributions import LowRankMultivariateNormal, Normal
 
 from Utilis import init_weights, init_weights_orthogonal_normal, l2_regularisation
 from torch.distributions import Normal, Independent, kl
@@ -15,7 +15,7 @@ class UNet_SCM(nn.Module):
     The segmentation network is U-net. The confusion  matrix network is defined in cm_layers
 
     """
-    def __init__(self, in_ch, resolution, batch_size, input_dim, width, depth, class_no, latent=512, norm='in'):
+    def __init__(self, in_ch, resolution, width, depth, class_no, latent=512, norm='in'):
         #
         # ===============================================================================
         # in_ch: dimension of input
@@ -31,51 +31,29 @@ class UNet_SCM(nn.Module):
         self.decoders = nn.ModuleList()
         self.encoders = nn.ModuleList()
 
-        # print(width*(2**(depth-1)))
-        # print(resolution // (2**(depth-1)))
-
-        # self.vae_encoder = scm_encoder(c=self.final_in, h=resolution, w=resolution, latent=latent)
-        self.vae_encoder = scm_encoder(c=self.final_in, h=resolution, w=resolution, latent=latent)
-        self.vae_decoder = scm_decoder(c=self.final_in, h=resolution, w=resolution, latent=latent)
+        self.cm_network = cm_net(c=width, h=resolution, w=resolution, class_no=class_no, latent=latent)
 
         for i in range(self.depth):
+
             if i == 0:
+
                 self.encoders.append(double_conv(in_channels=in_ch, out_channels=width, step=1, norm=norm))
                 self.decoders.append(double_conv(in_channels=width*2, out_channels=width, step=1, norm=norm))
+
             elif i < (self.depth - 1):
+
                 self.encoders.append(double_conv(in_channels=width*(2**(i - 1)), out_channels=width*(2**i), step=2, norm=norm))
                 self.decoders.append(double_conv(in_channels=width*(2**(i + 1)), out_channels=width*(2**(i - 1)), step=1, norm=norm))
+
             else:
+
                 self.encoders.append(double_conv(in_channels=width*(2**(i-1)), out_channels=width*(2**(i-1)), step=2, norm=norm))
                 self.decoders.append(double_conv(in_channels=width*(2**i), out_channels=width*(2**(i - 1)), step=1, norm=norm))
 
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         self.conv_last = nn.Conv2d(width, self.final_in, 1, bias=True)
 
-        self.conv_cm = nn.Conv2d(self.final_in, class_no**2, 1, bias=True)
-        # self.conv_last = seg_head(c=width, h=resolution, w=resolution, class_no=class_no, latent=latent)
-
-    def reparameterize(self, mu, logvar):
-        """
-        Will a single z be enough ti compute the expectation
-        for the loss
-        :param mu: (Tensor) Mean of the latent Gaussian
-        :param logvar: (Tensor) Standard deviation of the latent Gaussian
-        :return:
-        """
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-
-        return eps * std + mu
-
-    def sample(self, num_samples):
-        # num_samples will be used as the same shape as the z in forward function
-        z = torch.randn(num_samples)
-        z = z.to(device)
-        cms = self.scm_decoder(z)
-        return cms
-
-    def forward(self, x, gt):
+    def forward(self, x):
 
         y = x
         encoder_features = []
@@ -99,67 +77,75 @@ class UNet_SCM(nn.Module):
             y = torch.cat([y_e, y], dim=1)
             y = self.decoders[-(i+1)](y)
 
-        seg = self.conv_last(y)
-        # conditional_seg = torch.cat(seg, y)
-        if self.vae_encoder.training is True:
-            gt = gt.repeat(1, self.final_in, 1, 1)
-            mu, logvar = self.vae_encoder(gt)
-        else:
-            mu, logvar = self.vae_encoder(seg)
-        z = self.reparameterize(mu, logvar)
-        seg_sample = self.vae_decoder(z)
+        y_t = self.conv_last(y)
+        sampled_cm, mu, log_var = self.cm_network(y)
 
-        # seg = self.conv_last(y)
-        cm = self.conv_cm(seg)
-
-        return seg, seg_sample, cm, mu, logvar
+        return y_t, sampled_cm, mu, log_var
 
 
-class scm_encoder(nn.Module):
+
+class cm_net(nn.Module):
     """ This class defines the stochastic annotator network
     """
-    def __init__(self, c, h, w, latent):
-        super(scm_encoder, self).__init__()
-        self.fc_mu = nn.Linear(c*h*w, latent)
-        self.fc_var = nn.Linear(c*h*w, latent)
+    def __init__(self, c, h, w, class_no=2, latent=512):
+        super(cm_net, self).__init__()
+        self.fc_encoder = nn.Linear(c * h * w, 128)
+        self.fc_decoder = nn.Linear(latent, h * w * class_no ** 2)
+        self.fc_mu = nn.Linear(128, latent)
+        self.fc_var = nn.Linear(128, latent)
+
+        self.latent = latent
 
     def forward(self, x):
-        y = torch.flatten(x, start_dim=1)
-        mu = self.fc_mu(y)
-        var = self.fc_var(y)
-        return mu, var
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        # cm = F.relu(cm, inplace=True)
+        cm = self.decode(z)
+        return cm, mu, log_var
 
+    def encode(self, input):
+        res = torch.flatten(input, start_dim=1)
+        res = F.relu(self.fc_encoder(res))
 
-class scm_decoder(nn.Module):
-    """ This class defines the annotator network, which models the confusion matrix.
-    Essentially, it share the semantic features with the segmentation network, but the output of annotator network
-    has the size (b, c**2, h, w)
-    """
-    def __init__(self, c, h, w, latent=512):
-        super(scm_decoder, self).__init__()
-        self.w = w
-        self.h = h
-        self.c = c
-        self.mlp_seg = nn.Linear(latent, h*w*c)
+        # mu and var components of the latent gaussian distribution
+        mu = self.fc_mu(res)
+        log_var = self.fc_var(res)
 
-    def forward(self, x):
-        y = self.mlp_seg(x)
-        return y.reshape(-1, self.c, self.h, self.w)
+        return mu, log_var
 
-#
-# class seg_head(nn.Module):
-#     def __init__(self, c, h, w, class_no, latent):
-#         super(seg_head, self).__init__()
-#         self.encoder = nn.Linear(c*h*w, latent)
-#         self.decoder = nn.Linear(latent, h*w*class_no**2)
-#
-#     def forward(self, x):
-#         y = torch.flatten(x, start_dim=1)
-#         y = self.encoder(y)
-#         y = F.relu(y, inplace=True)
-#         y = self.decoder(y)
-#         return y
+    def decode(self, z):
+        """
+        maps latent codes onto the image space
+        """
+        res = self.fc_decoder(z)
+        res = F.softplus(res)
+        return res
 
+    def reparameterize(self, mu, log_var):
+        """
+        Reparameterization trick to sample from N(mu, var) from
+        N(0,1).
+
+        Args:
+            mu (_type_): _description_
+            log_var (_type_): _description_
+        """
+        std = torch.exp(0.5*log_var)
+        eps = torch.randn_like(std)
+        return eps*std + mu
+
+    def sample(self, num_samples):
+        """
+        Samples from the latent space and return the corresponding
+        image space map.
+
+        Args:
+            num_samples (_type_): _description_
+        """
+        z = torch.randn(num_samples, self.latent)
+        z = z.to(device)
+        samples = self.decode(z)
+        return samples
 
 def double_conv(in_channels, out_channels, step, norm):
     # ===========================================
