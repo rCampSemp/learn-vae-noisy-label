1423df5504b6a97b9e5704597d64d514b8697f3b
rCampSemp <semp.rhys@outlook.com>
2022-09-06 17:28:32 +0100

    2022 06 09 b: update LIDC and MNIST script

--- preprocessing/Prepare_LIDC.py
+++ preprocessing/Prepare_LIDC.py
@@ -11,7 +11,21 @@ from collections import defaultdict
 import cv2 as cv
 
 class PrepareLIDC:
+    """Class for preprocessing and saving our LIDC dataset with its' metadata.
+
+    Args:
+        LIDC_path (str): path to full unprocessed LIDC data with subfolders of DICOM files
+        save_path (str): file path to save all data to
+        clevel (float): agreement level of consensus consolidation
+        mask_threshold (2-tuple of int): 1- lower limit of mask sizes we allow  2- upper limit of mask sizes we allow
+        resolution (int): size of the resulting images we want, will result in saving images of size resolution x resolution
+        meta_keys (list): list of str that are headers of parameters of the metadata associated with lidc data
+        hist (str, optional): indicates type of regime to apply to CT scans
+        cliplimit (float, optional): clip limit for CLAHE histogram equalization. Defaults to 0.2
+    """
     def __init__(self, LIDC_path, save_path, clevel, mask_threshold, resolution, meta_keys, hist=None, cliplimit=0.2) -> None:
+        """Constructor for PrepareLIDC class.
+        """
         self.LIDC_path = LIDC_path
         self.save_path = save_path
 
@@ -28,17 +42,15 @@ class PrepareLIDC:
         self.cliplimit = cliplimit
 
     def config(self):
-        """Function to write LIDC data location to config file for pylidc.
-
-        Args:
-            path_to_lidc (str): absoluate file path to LIDC dataset parent folder 
-        """
+        """Function to write LIDC data location to config file for pylidc."""
         # write path to pylidc config file
         f = open('/home/rhys/.pylidcrc', 'w')
         f.write(f'[dicom]\npath = {self.LIDC_path}\nwarn = True\n\n')
         f.close()
 
     def make_folders(self):
+        """Creates train/test/validate folders and their subfolders.
+        """
         os.makedirs(self.save_path, exist_ok=True)
 
         self._make_split_folder('train')
@@ -49,6 +61,11 @@ class PrepareLIDC:
         os.makedirs(meta_path, exist_ok=True)
     
     def _make_split_folder(self, split):
+        """makes subfolder for make_folders method
+
+        Args:
+            split (str): str to indicate type of subfolder to create
+        """
         annot_path = self.save_path + '/' + split + '/masks/annots'
         truth_path = self.save_path + '/' + split + '/masks/GT'
         scan_path = self.save_path + '/' + split + '/scans'
@@ -58,16 +75,18 @@ class PrepareLIDC:
         os.makedirs(scan_path, exist_ok=True)
 
     def _get_patients(self, lidc_path):
-        for fname in glob.glob(lidc_path + 'LIDC-IDRI-*'):
+        """Collects all patient ids available and shuffles in random order. 
+
+        Args:
+            lidc_path (str): path to unprocessed LIDC data 
+        """
+        for i, fname in enumerate(glob.glob(lidc_path + 'LIDC-IDRI-*')):
             self.patient_ids.append(fname[-14:])
         
         random.shuffle(self.patient_ids)
 
     def train_test_val_split(self):
         """Splits input file path into train/test/validation splits based on ratio.
-
-        Args:
-            allFiles (list[str]): list of strings representing file paths to data
 
         Returns:
             lists of ndarrays: lists of sub-arrays representing train/test/validation split of input file paths
@@ -88,7 +107,6 @@ class PrepareLIDC:
 
         Args:
             bbox (tuple of slice objects): 3-tuple of Python slice objects around nodule of interest 
-            dim (int): desired dimension to resize bbox to e.g. dim=64 corresponds to scan volume size 64x64
 
         Returns:
             tuple: 3-tuple of Python slice objects to index the image volume in desired dim size
@@ -110,107 +128,132 @@ class PrepareLIDC:
         return slice(bboxx1, bboxx2), slice(bboxy1, bboxy2), bbox[2]
 
     def getAverageParams(self, annots):
-        """_summary_
+        """Calculates list of average parameters from metadata on LIDC data from keylist
 
         Args:
-            annots (:obj: 'list' of :obj: 'pylidc.Annotation'): list of pylidc.Annotation objects that refer to the same physical nodule in the scan
+            annots (list of pylidc.Annotation's): list of pylidc.Annotation objects that refer to the same physical nodule in the scan
             params: Variable length list of meta information keys
 
         Returns:
             list of floats: list of average scores for each annotation for a nodule correspond to their keys in *params
         """
+        # iterate over keylist
         averages = []
         for param in self.keylist[2:]:
             tmp_score = 0
             for annot in annots:
-                tmp_score += getattr(annot, param)
-        
+                tmp_score += getattr(annot, param) # get value of parameter from keylist from anntotation
+
+            # calculate average and add to list
             averages.append(round(tmp_score/len(annots)))
 
         return averages
 
     def _standardize(self, img):
+        """Z-score standardisation of img input.
+
+        Args:
+            img (numpy array): unnormalised scan image of lidc 
+
+        Returns:
+            std_img (numpy array): standardised img array
+        """
         std_img = ( img - img.mean(axis=(0,1), keepdims=True) ) / img.std(axis=(0,1), keepdims=True)
         return std_img
 
-    def zeroto255(self, img):
-        new_img = ( ( img - img.min() ) * ( 1/(img.max() - img.min() ) * 255) ).astype(np.uint8)
-        return new_img
+    # def zeroto255(self, img):
+    #     new_img = ( ( img - img.min() ) * ( 1/(img.max() - img.min() ) * 255) ).astype(np.uint8)
+    #     return new_img
 
-    def clahe_fn(self, img):
-        new_img = ( ( img - img.min() ) * ( 1/(img.max() - img.min() ) * 255) ).astype(np.uint8)
-        clahe = cv.createCLAHE(clipLimit=self.cliplimit, tileGridSize=(8,8))
-        new_img = clahe.apply(new_img)
-        return new_img
+    # def clahe_fn(self, img):
+    #     new_img = ( ( img - img.min() ) * ( 1/(img.max() - img.min() ) * 255) ).astype(np.uint8)
+    #     clahe = cv.createCLAHE(clipLimit=self.cliplimit, tileGridSize=(8,8))
+    #     new_img = clahe.apply(new_img)
+    #     return new_img
 
-    def equal_hist(self, img):
-        new_img = ( ( img - img.min() ) * ( 1/(img.max() - img.min() ) * 255) ).astype(np.uint8)
-        eq_img = cv.equalizeHist(new_img)
-        return eq_img
+    # def equal_hist(self, img):
+    #     new_img = ( ( img - img.min() ) * ( 1/(img.max() - img.min() ) * 255) ).astype(np.uint8)
+    #     eq_img = cv.equalizeHist(new_img)
+    #     return eq_img
 
     def save_ims(self, data, path_to_split):
-        """_summary_
+        """Main loop to save images and binary masks.
 
         Args:
-            split_data (_type_): _description_
-            truepath (_type_): _description_
-            annotpath (_type_): _description_
-            scanpath (_type_): _description_
-        """        
+            data (list): list of str denoting patient IDs to save
+            path_to_split (str): folder path to scans to save
+        """       
+        # create subfolder file paths  
         annotpath = path_to_split + '/masks/annots'
         truepath = path_to_split + '/masks/GT'
         scanpath = path_to_split + '/scans'
 
+        # iterate over each patient in data list
         for pid in data:
+            
+            # query for CT scans with patient id = pid and convert to an array volume
             scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == pid).first()
             vol = scan.to_volume() 
+
+            # cluster the annotations of the scan
             nods = scan.cluster_annotations()
-            #
+            
+            # if nodules are present
             if len(nods) > 0:
 
+                # iterate over all nodules in cluster
                 for nod_idx, nod in enumerate(nods):
-                    # get consensus masks and bounding box 
+
+                    # get consensus, bounding box and annotations
                     cmask,cbbox,masks = consensus(nod, clevel=self.clevel)
-                    #
-                    # ignore nodules too large
+                    
+                    # require more than one annotation per nodule
+                    if len(masks) < 2:
+                        continue
+                    
+                    # ignore nodules too large and unable to rescale to size
                     if cmask.shape[0] > self.res or cmask.shape[1] > self.res:
                         continue
-                    #
+                    
                     # reshape bounding box to given dimensions
                     cbbox2 = self.reshape(cbbox)
                     scan_img = vol[cbbox2]
-                    #
+
+                    # some scans have no slices need to be ignored
                     numslices = scan_img.shape[2]
-                    # some scans have no slices
                     if numslices <= 0:
                         continue
-                    #
-                    # add channel dim to data  
-                    # scan_img = np.expand_dims(scan_img, axis=0)
+                    
+                    # add channel dimension to data  
                     cmask = np.expand_dims(cmask, axis=0)
                     for annot in masks:
                         annot.resize(1, cmask.shape[1], cmask.shape[2], numslices, refcheck=False) #refcheck false to allow resizing referenced array
-                    #
-                    #
+                    
                     # stack each annotation from list of annotations to c x h x w x annots x slices
                     masks = np.stack(masks, axis=3)
-                    #
+                    
                     # pad masks into given dimensions
                     cmask = np.pad(cmask, pad_width=((0,0), (cbbox[0].start - cbbox2[0].start, cbbox2[0].stop - cbbox[0].stop), (cbbox[1].start - cbbox2[1].start, cbbox2[1].stop - cbbox[1].stop), (0,0)), mode='constant', constant_values=False)
                     masks = np.pad(masks, pad_width=((0,0), (cbbox[0].start - cbbox2[0].start, cbbox2[0].stop - cbbox[0].stop), (cbbox[1].start - cbbox2[1].start, cbbox2[1].stop - cbbox[1].stop), (0,0), (0,0)), mode='constant', constant_values=False)
-                    #
+                    
                     # save metadata to dict
                     averages = self.getAverageParams(nod)
                     data = [pid[-4:], nod_idx] + averages
                     for i, key in enumerate(self.keylist):
                         self.metadata[key].append(data[i])
+                    
+                    # empty list to hold nodule sizes for future analysis
                     nod_size_list = []
-                    #   
+
+                    #   iterate over slices
                     for slice_idx in range(numslices):
                         nod_size = np.sum(cmask[:,:,:,slice_idx])
 
+                        # skip slice if nodule size is too large or small or if scan image or GT does not fit resolution
                         if nod_size <= self.mask_threshold or nod_size >= 800 or cmask.shape[1] != self.res or cmask.shape[2] != self.res or scan_img.shape[0] != self.res or scan_img.shape[1] != self.res:
                             continue
+
+                        # append nodule size to list
                         nod_size_list.append(nod_size)
 
                         # save ground truth masks
@@ -218,14 +261,16 @@ class PrepareLIDC:
                                                                 + '_slice_' + str(slice_idx) + '.tif')
                         imsave(full_store_path_true, cmask[:,:,:,slice_idx])
                     
-                        #save scan of lung
+                        ## save scan of lung
+                        # method to process scan img
                         if self.hist is None:
                             new_scan_img = self._standardize(scan_img[:,:,slice_idx])
                         elif self.hist == 'clahe':
                             new_scan_img = self.clahe_fn(scan_img[:,:,slice_idx])
                         elif self.hist == 'equal':                            
                             new_scan_img = self.equal_hist(scan_img[:,:,slice_idx])
-
+                        
+                        # add channel dimension to img
                         new_scan_img = np.expand_dims(new_scan_img, axis=0)
                         full_store_path_scan = os.path.join(scanpath, 'pid_' + pid[-4:] + '_nod_' + str(nod_idx) 
                                                                 + '_slice_' + str(slice_idx) + '.tif')
@@ -236,9 +281,12 @@ class PrepareLIDC:
                                                                 + '_slice_' + str(slice_idx) + '.tif')
                         imsave(full_store_path_annot, masks[:,:,:,:,slice_idx])
 
+                    # add nodule sizes to metadata
                     self.metadata['nod_sz_per_slice'].append(nod_size_list)
     
     def save_meta_csv(self):
+        """Saves metadata as scv
+        """
         meta_df = pd.DataFrame.from_dict(self.metadata)
         meta_df.sort_values(by=['patient_id', 'nodule_no'], inplace=True)
         df_filename = 'metadata.csv'
@@ -247,6 +295,8 @@ class PrepareLIDC:
         print('metadata saved')
 
     def prepare_data(self):
+        """Main function to save all data and metadata
+        """
         self.config()
         self.make_folders()
 
@@ -260,12 +310,16 @@ class PrepareLIDC:
 
     
 if __name__ == '__main__':
+    # keylist parameters taken from pylidc documentation
     keylist = ['patient_id','nodule_no','subtlety', 'internalStructure', 'calcification', 'sphericity', 'margin', 'lobulation', 'spiculation', 'texture', 'malignancy']
 
+    # path to unprocessed lidc data
     path_lidc = '/home/rhys/Documents/datasets/full_LIDC/LIDC-IDRI/'
+    # save path where saved data will end up
     save_path = '../LIDC_examples'
 
-    lidc_process = PrepareLIDC(path_lidc, save_path, clevel=0.5, mask_threshold=30, resolution=64, meta_keys=keylist, hist=None)
+    # save eeverything
+    lidc_process = PrepareLIDC(path_lidc, save_path, clevel=0.5, mask_threshold=(30, 800), resolution=64, meta_keys=keylist, hist=None)
     lidc_process.prepare_data()
 
     print('End')
--- preprocessing/Prepare_MNIST.py
+++ preprocessing/Prepare_MNIST.py
@@ -5,42 +5,40 @@ import cv2
 import numpy as np
 from tifffile import imsave
 
-trainset = MNIST('../../MNIST_data/Train', train=True, download=True)
-testset = MNIST('../../MNIST_data/Test', train=False, download=True)
 
-def divide_cases(trainset, testset):
+def divide_cases(trainset, test_set):
     """Divides mnist datasets into train val and test split datasets.
 
     Args:
-        trainset (_type_): MNIST dataset train
-        testset (_type_): MNIST dataset test
+        trainset (torchvision.datasets.MNIST): MNIST train dataset train
+        testset (torchvision.datasets.MNIST): MNIST test dataset
 
     Returns:
-            - train_set (subset)
-            - val_set (subset)
-            - testset (Dataset)
+        train_set (Subset object): subset of MNIST dataset used for training
+        val_set (Subset object): subset of MNIST dataset used for validation
+        test_set (Subset object): subset of MNIST dataset used for testing
     """
 
     train_set, val_set = torch.utils.data.random_split(trainset, [50000, 10000])
-    return train_set, val_set, testset
-
-def split_modes(dataset, split_no=5):
-    data_len = len(dataset)
-    split_list = [int(data_len/split_no) for _ in range(split_no-1)] 
-    # final split to sum to len(dataset)
-    split_list_final = split_list + [data_len - sum(split_list)]
-    modes = torch.utils.data.random_split(dataset, split_list_final)
-    return modes
-# guassian 0-1, masks 0 or 1
-
-train_modes, val_modes, test_modes = divide_cases(trainset, testset)
-
+    return train_set, val_set, test_set
 
 def gauss_transform(img, mean=2, std=5, weight=0.5):
-    # img, label
+    """Function to transform MNIST img to img with guassian noise added and normalised.
+
+    Args:
+        img (numpy array): input img array
+        mean (int, optional): mean of guassian distribution. Defaults to 2.
+        std (int, optional): standard deviation of guassian distrbution. Defaults to 5.
+        weight (float, optional): weight of blending of the input img with gaussian noise. Defaults to 0.5.
+
+    Returns:
+        img (numpy array): _description_
+    """
     img = img.copy()
+    # generate gaussian noise in same shape as input img
     gaussian = np.uint8(np.random.normal(mean, std, (img.shape[0],img.shape[1])))
-    # 0 - 255 to 0 - 1 range
+    
+    # weighted addition of gaussian noise with img, 0 - 255 => 0 - 1 range
     img = cv2.addWeighted(img, 1.0, gaussian, weight, 0) / 255
     return img
 
@@ -48,14 +46,15 @@ def main(data, save_path, split):
     """main process
 
     Args:
-        data (_type_): _description_
-        save_path (_type_): _description_
-        split (_type_): _description_
+        data (Subset object): subset of MNIST dataset
+        save_path (str): file path to save processed MNIST data
+        split (str): subfolder to save data to, train/validate/test
     """
-
+    # file path depending on data type to save data to
     save_path = save_path + split
     os.makedirs(save_path, exist_ok=True)
     
+    # initialize subfolders to save data to
     save_gauss = save_path + '/Gaussian'
     save_GT = save_path + '/GT'
     save_over = save_path + '/Over'
@@ -68,36 +67,44 @@ def main(data, save_path, split):
     os.makedirs(save_under, exist_ok=True)
     os.makedirs(save_wrong, exist_ok=True)
 
+    # kernel for morphological operations
     kernel = np.ones((2, 2), np.uint8)
     
-    # labels not needed
+    # iterate over given subset with labels not needed
     for idx, (img, _) in enumerate(data):
         # opencv compatible type
         img = np.asarray(img, dtype=np.uint8)
 
+        # get gaussian img transformation
         gauss_img = gauss_transform(img)
         # make gauss img 3 channels for compatibility with customdataset_punet
         gauss_img = gauss_img.reshape((28, 28, 1)).repeat(3, -1) # repeat the last (-1) dimension three times
 
+        # under and over annotators
         under_img = cv2.erode(img, kernel, iterations=2)
         over_img = cv2.dilate(img, kernel, iterations=2)
 
-        # morpological operations to produce wrong segmentation
+        ## wrong segmentation production
         rows, cols = img.shape
-        M = cv2.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),135,1)
+
+        # rotate img 135 degrees
+        M = cv2.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),135,1) 
         wrong_img = cv2.warpAffine(img,M,(cols,rows))
+
+        # morpological operations to produce wrong segmentation
         wrong_img = cv2.morphologyEx(wrong_img, cv2.MORPH_TOPHAT, kernel)
         wrong_img = cv2.dilate(wrong_img, kernel, iterations=4)
         wrong_img = cv2.morphologyEx(wrong_img, cv2.MORPH_TOPHAT, kernel)
         wrong_img = cv2.dilate(wrong_img, kernel, iterations=4)
 
+        # save name for each subfolder
         save_gauss_file = save_gauss + '/' + str(idx) + '_Gaussian.tif'
         save_under_file = save_under + '/' + str(idx) + '_Under.tif'
         save_over_file = save_over + '/' + str(idx) + '_Over.tif'
         save_wrong_file = save_wrong + '/' + str(idx) + '_Wrong.tif'
         save_GT_file = save_GT + '/' + str(idx) + '_GT.tif'
 
-
+        # threshold images to make binary mask
         (_, wrong_img) = cv2.threshold(wrong_img, 50, 1,
             cv2.THRESH_BINARY)
         (_, under_img) = cv2.threshold(under_img, 0, 1,
@@ -107,6 +114,7 @@ def main(data, save_path, split):
         (_, GT_img) = cv2.threshold(img, 0, 1,
             cv2.THRESH_BINARY)
 
+        # save masks to subfolders
         imsave(save_gauss_file, gauss_img)
         imsave(save_GT_file, GT_img)
         imsave(save_over_file, over_img)
@@ -114,11 +122,17 @@ def main(data, save_path, split):
         imsave(save_wrong_file, wrong_img)
 
 if __name__ == '__main__':
+    # get MNIST datasets
     trainset = MNIST('../../MNIST_data/Train', train=True, download=True)
     testset = MNIST('../../MNIST_data/Test', train=False, download=True)
 
+    # divide data
     train, val, test = divide_cases(trainset, testset)
+
+    # where to save data
     save_path = '../MNIST_data'
+
+    # main loop for each data type
     main(train, save_path, '/train')
     main(val, save_path, '/validate')
     main(test, save_path, '/test')
