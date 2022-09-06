a7ebb98037e4f4765e4421e3df17dbf914501034
rCampSemp <semp.rhys@outlook.com>
2022-09-07 00:52:09 +0100

    2022 09 07 d: adds image analysis notebook

--- Models.py
+++ Models.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from Utilis import init_weights, init_weights_orthogonal_normal, l2_regularisation
 from torch.distributions import Normal, Independent, kl
-
+import numpy as np
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
